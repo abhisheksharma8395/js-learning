155fd0b93d291cfd90f5030d6edcf5cf5bb8c006
Abhishek Sharma <abhiparashar8630@gmail.com>
2025-01-23 21:32:55 +0530

    calculating Gst on course

---
+++ 01_basics/gstCalculation.js
@@ -0,0 +1,5 @@
+let pwWebDCoursePrice = 2000;
+let gstOnEducationCourses = 0.18;
+console.log("The total price user have to pay to buy the Web Development course is",pwWebDCoursePrice+(pwWebDCoursePrice*gstOnEducationCourses),"rupess only.");
+let pwDSACoursePrice = 2500;
+console.log("The total price user have to pay to buy the DSA course is",pwDSACoursePrice+(pwDSACoursePrice*gstOnEducationCourses),"rupess only.");
