7b042ba5b81fc82c81b93d65c8e600c47162c549
Abhishek Sharma <abhiparashar8639@gmail.com>
2025-08-24 18:34:21 +0530

    Learns about Type Coercion

--- 02_DataTypesAndTypeSystem/05_TypeCoercion.js
+++ 02_DataTypesAndTypeSystem/05_TypeCoercion.js
@@ -1,7 +1,7 @@
 // Type Coercion ( == vs === )
 // Truthy vs Falsy values
 
-/* type coercion -> type coercion is the automatic or implicit conversion of values from 
+/* Type Coercion -> type coercion is the automatic or implicit conversion of values from 
 one data type to another (such as strings to numbers).
 */
 
@@ -9,3 +9,10 @@ let num = 5; // number
 let str = "5"; // string
 console.log(num == str); // true, because of type coercion
 console.log(num === str); // false, no type coercion, different types
+
+
+// Falsy values in JavaScript -> false, 0, "", null, undefined, NaN, Document.all
+
+console.log(!!0); // false
+console.log(!!1); // true
+
