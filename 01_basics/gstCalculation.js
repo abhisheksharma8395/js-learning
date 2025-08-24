a2343899b30e2e00216b225c3a4cf6470fc07166
Abhishek Sharma <abhiparashar8639@gmail.com>
2025-08-24 12:44:01 +0530

    update the file

--- 01_basics/gstCalculation.js
+++ 01_basics/gstCalculation.js
@@ -1,5 +1,5 @@
-let pwWebDCoursePrice = 2000;
+let DevCoursePrice = 2000;
 let gstOnEducationCourses = 0.18;
-console.log("The total price user have to pay to buy the Web Development course is",pwWebDCoursePrice+(pwWebDCoursePrice*gstOnEducationCourses),"rupess only.");
-let pwDSACoursePrice = 2500;
-console.log("The total price user have to pay to buy the DSA course is",pwDSACoursePrice+(pwDSACoursePrice*gstOnEducationCourses),"rupess only.");
+console.log("The total price user have to pay for buying the Web Development course is",DevCoursePrice+(DevCoursePrice*gstOnEducationCourses),"rupess only.");
+let DSACoursePrice = 2500;
+console.log("The total price user have to pay for buying the DSA course is",DSACoursePrice+(DSACoursePrice*gstOnEducationCourses),"rupess only.");
